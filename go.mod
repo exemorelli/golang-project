bf9e531e75fddf2f2a3cb0378c5083730f5e64ad
Exequiel Leandro Morelli <exemorelli@gmail.com>
2024-02-14 21:54:29 -0300

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/exemorelli/golang-proyect
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/exemorelli/golang-proyect/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
